1120d984fa5881513fefa1e256158193d8a986b8
brian greenfield <briang@cpan.org>
2018-08-31 14:43:31 +0100

    bump version

--- lib/Bit/Twiddling.pm
+++ lib/Bit/Twiddling.pm
@@ -9,11 +9,11 @@ Bit::Twiddling - Low-level bit-twiddling hacks
 
 =head1 VERSION
 
-Version 0.02 - 20180831
+Version 0.03 - 2018-08-31
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 
